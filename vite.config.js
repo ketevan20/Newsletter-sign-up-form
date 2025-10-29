8e9a8dea06a1681be79323ef89ab4c43334d1d4d
ketevan20 <ketevansamukashvili@gmail.com>
2025-10-30 01:14:08 +0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/newsletter-sign-up-form/',
+  base: '/Newsletter-sign-up-form/',
   plugins: [
     react({
       babel: {
