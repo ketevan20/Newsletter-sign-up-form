d0c1786190f1885576bceaedcdc9b02eb37575c4
ketevan20 <ketevansamukashvili@gmail.com>
2025-10-30 00:13:09 +0400

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
+import Card from './components/Card'
 import './App.css'
+import SuccessMessage from './components/SuccessMessage';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [imageToRender, setImage] = useState('/images/illustration-sign-up-desktop.svg');
+  const [email, setEmail] = useState('');
+  const [sent, setSent] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 700) {
+        setImage('/images/illustration-sign-up-mobile.svg');
+      } else {
+        setImage('/images/illustration-sign-up-desktop.svg');
+      }
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth < 700) {
+        setImage('/images/illustration-sign-up-mobile.svg');
+      } else {
+        setImage('/images/illustration-sign-up-desktop.svg');
+      }
+    })
+  }, [])
+
+  function DisplayMessage(email) {
+    setEmail(email);
+    setSent(!sent);
+  }
+
+  function ReturnBack() {
+    setSent(!sent);
+    setEmail('');
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {email ? <SuccessMessage email={email} handleFunction={ReturnBack}/> : ''}
+      {!sent ? <Card
+        title="Stay updated!"
+        text="Join 60,000+ product managers receiving monthly updates on:"
+        listIcon="/images/icon-list.svg"
+        listText1="Product discovery and building what matters"
+        listText2="Measuring to ensure updates are a success"
+        listText3="And much more!"
+        inputText="Email address"
+        placeholder="email@company.com"
+        buttonText="Subscribe to monthly newsletter"
+        image={imageToRender}
+        handleFucntion={DisplayMessage}
+      /> : ''}
+    </div>
   )
 }
 
---
+++ src/components/SuccessMessage.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const SuccessMessage = (props) => {
+  function HandleOnClick() {
+    props.handleFunction();
+  }
+
+  return (
+    <div className='success-container'>
+      <img src='/images/icon-success.svg' />
+      <h1>Thanks for subscribing!</h1>
+      <p>
+        A confirmation email has been sent to <span style={{fontWeight: 'bold'}}>{props.email}. </span> 
+        Please open it and click the button inside to confirm your subscription.
+      </p>
+      <button onClick={HandleOnClick}>Dismiss message</button>
+    </div>
+  )
+}
+
+export default SuccessMessage
